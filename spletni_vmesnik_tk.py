c4989172fb6377a259d149a8b967d92f50ab666a
MartinZust123 <115585936+MartinZust123@users.noreply.github.com>
2023-05-02 22:24:40 +0200

    Dodal datoteke
    Datoteke, ki sem jih dodal tvorijo osnovno arhitekturo projekta oz. spletne aplikacije

---
+++ spletni_vmesnik_tk.py
@@ -0,0 +1,16 @@
+import bottle
+# from model_tekaski import Parkirišče
+
+@bottle.get("/")
+def prvi_zaslon():
+    return bottle.template("zacetna_stran_tk.html")
+
+@bottle.get("/rezultati/")
+def rezultati_tekov():
+    return bottle.template("rezultati.html")
+
+@bottle.get("/kalkulator/")
+def vrni_kalkulator():
+    return bottle.template("kalkulator.html")
+
+bottle.run(reloader=True)
