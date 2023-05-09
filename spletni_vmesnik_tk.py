e8178da860da62bdced8d671ee68d66e2df470a0
Martin Žust <martin.zust@student.fmf.uni-lj.si>
2023-05-09 19:18:18 +0200

    Dodatno delo na forntendu

--- spletni_vmesnik_tk.py
+++ spletni_vmesnik_tk.py
@@ -14,9 +14,16 @@ def rezultati_tekov():
 def vrni_kalkulator():
     return bottle.template("kalkulator.html")
 
+@bottle.get("/kraski22_10m/")
+def kraski22_10():
+    return bottle.template("2022_kraski_10_Mn.html")
+
+@bottle.get("/kraski22_10z/")
+def kraski22_10z():
+    return bottle.template("2022_kraski_10_Zn.html")
+
 @bottle.get("/static/<filepath:path>")
 def vrni_staticno_datoteko(filepath):
     return bottle.static_file(filepath, 'static')
-
 
 bottle.run(reloader=True)
