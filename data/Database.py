07f1be13450d201f3b314f9e2d04c7990375a38f
Matej Gorjup <matej.gorjup123@gmail.com>
2023-05-28 21:23:19 +0200

    Začel urejati modele

--- data/Database.py
+++ data/Database.py
@@ -1,12 +1,26 @@
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
-from re import sub
 import os
 import data.auth_public as auth
+
+# za izdelat dataclasse za posamezne izpise
 
 class Repo:
 
     def __init__(self):
         self.conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=5432)
         self.cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+     def dobi_gen(self, typ: Type[T], take=30, skip=0) -> List[T]:
+        """ 
+        Generična metoda, ki za podan vhodni dataclass vrne seznam teh objektov iz baze.
+        Predpostavljamo, da je tabeli ime natanko tako kot je ime posameznemu dataclassu.
+        """
+
+
+        # ustvarimo sql select stavek, kjer je ime tabele typ.__name__ oz. ime razreda
+        tbl_name = typ.__name__
+        sql_cmd = f'''SELECT * FROM {tbl_name} LIMIT {take} OFFSET {skip};'''
+        self.cur.execute(sql_cmd)
+        return [typ.from_dict(d) for d in self.cur.fetchall()]
