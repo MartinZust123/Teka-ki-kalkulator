3a96d1677be9f7792fd14abe55d5a5eae2deb90f
Matej Gorjup <matej.gorjup123@gmail.com>
2023-05-28 21:23:38 +0200

    Začel urejati modele

--- data/Modeli.py
+++ data/Modeli.py
@@ -5,6 +5,10 @@ from dataclasses_json import dataclass_json
 @dataclass
 class Uporabnik:
     username: str = field(default="")
-    role: str = field(default="")
     password_hash: str = field(default="")
     last_login: str = field(default="")
+
+@dataclass
+class Rezultat:
+    id: int = field(default=0)
+    uvrstitev: int = field(default=0)
