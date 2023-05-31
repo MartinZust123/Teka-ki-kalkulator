522f63daa11d58a5f92a538c6a00a5945ba6e68f
Matej Gorjup <matej.gorjup123@gmail.com>
2023-05-31 23:11:20 +0200

    Dodal dataclasse

--- data/Modeli.py
+++ data/Modeli.py
@@ -4,11 +4,35 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class Uporabnik:
-    username: str = field(default="")
-    password_hash: str = field(default="")
-    last_login: str = field(default="")
+    uporabnisko_ime: str = field(default="")
+    geslo_hash: str = field(default="")
+    login: str = field(default="")
+    spol: str = field(default="")
+    datum_rojstva: str = field(default="")
+    rang_tekaca: str = field(default="")
 
+@dataclass_json
+@dataclass
+class Tek:
+    id: int = field(default="")
+    vrsta_teka: str = field(default="")
+    razdalja: int = field(default="")
+    cas: int = field(default="")
+
+@dataclass_json
+@dataclass
+class Tekmovanje:
+    id: int = field(default="")
+    naziv: str = field(default="")
+    leto: int = field(default="")
+    razdalja: int = field(default="")
+
+@dataclass_json
 @dataclass
 class Rezultat:
-    id: int = field(default=0)
-    uvrstitev: int = field(default=0)
+    id: int = field(default="")
+    tekmovanje = str = field(default="")
+    priimek_ime: str = field(default="")
+    rezultat: str = field(default="")
+
+
