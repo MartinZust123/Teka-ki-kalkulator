73857b88857ed18b90f5f5fe00f1f21fecfb6c2c
Matej Gorjup <matej.gorjup123@gmail.com>
2023-08-20 19:41:29 +0200

    Popravil modele

--- data/Modeli.py
+++ data/Modeli.py
@@ -12,27 +12,58 @@ class RangTekaca:
 class Uporabnik:
     uporabnisko_ime: str = field(default="")
     geslo_hash: str = field(default="")
-    login: str = field(default="")
     spol: str = field(default="")
-    datum_rojstva: str = field(default="")
+    leto_rojstva: int = field(default=0)
     rang_tekaca: RangTekaca = field(default="")
 
-@dataclass_json
-@dataclass
-class UporabnikDto:
-    uporabnisko_ime: str = field(default="")
+#@dataclass_json
+#@dataclass
+#class UporabnikDto:
+#    uporabnisko_ime: str = field(default="")
 
 @dataclass_json
 @dataclass
-class Cas:
-    cas: str = field(default=0)
-    izbrano: bool = field(default=False)
+class Tekmovanje:
+    # drugi classi za čas? 
+    id: int = field(default=0)
+    spol: str = field(default="")
+    priimekinime: str = field(default="")
+    lr: int = field(default=0)
+    km_tt: str = field(default="")
+    km_ss: str = field(default="")
+    km_deset: str = field(default="")
+    km_tri: str = field(default="")
 
-@dataclass_json
-@dataclass
-class Razdalja:
+    kraj: str = field(default="")
+    leto: int = field(default=0)
     razdalja: int = field(default=0)
-    izbrano: bool = field(default=False)
+
+    km_sest: str = field(default="")
+    km_sedemnajst: str = field(default="")
+    km_enaindvajset: str = field(default="")
+    km_pet: str = field(default="")
+    km_petnajst: str = field(default="")
+    km_dvajset: str = field(default="")
+    km_polmaraton: str = field(default="")
+    km_petindvajset: str = field(default="")
+    km_trideset: str = field(default="")
+    km_petintrideset: str = field(default="")
+    km_stirideset: str = field(default="")
+    km_maraton: str = field(default="")
+    km_dva: str = field(default="")
+
+
+#@dataclass_json
+#@dataclass
+#class Cas:
+#    cas: str = field(default=0)
+#    izbrano: bool = field(default=False)
+
+#@dataclass_json
+#@dataclass
+#class Razdalja:
+#    razdalja: int = field(default=0)
+#    izbrano: bool = field(default=False)
 
 @dataclass_json
 @dataclass
@@ -46,21 +77,21 @@ class Tek:
     id: int = field(default=0)
     tekac: Uporabnik = field(default="")
     vrsta_teka: VrstaTeka = field(default="")
-    razdalja: Razdalja = field(default="")
-    cas: Cas = field(default="")
+    razdalja: int = field(default=0)
+    cas: int = field(default=0)
 
-@dataclass_json
-@dataclass
-class Tekmovanje:
-    id: int = field(default=0)
-    naziv: str = field(default="")
-    leto: int = field(default=0)
-    razdalja: int = field(default="")
+#@dataclass_json
+#@dataclass
+#class Tekmovanje:
+#    id: int = field(default=0)
+#    naziv: str = field(default="")
+#    leto: int = field(default=0)
+#    razdalja: int = field(default="")
 
-@dataclass_json
-@dataclass
-class Rezultat:
-    id: int = field(default=0)
-    tekmovanje: Tekmovanje = field(default="")
-    priimek_ime: str = field(default="")
-    rezultat: str = field(default="")
+#@dataclass_json
+#@dataclass
+#class Rezultat:
+#    id: int = field(default=0)
+#    tekmovanje: Tekmovanje = field(default="")
+#    priimek_ime: str = field(default="")
+#    rezultat: str = field(default="")
