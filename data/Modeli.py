b2cdc54fe3445733cca094fffda5a7034ac70066
Matej Gorjup <matej.gorjup123@gmail.com>
2023-08-20 18:00:43 +0200

    Popravki default

--- data/Modeli.py
+++ data/Modeli.py
@@ -4,7 +4,7 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class RangTekaca:
-    id: int = field(default="")
+    id: int = field(default=0)
     rang: str = field(default="")
 
 @dataclass_json
@@ -20,25 +20,25 @@ class Uporabnik:
 @dataclass_json
 @dataclass
 class Cas:
-    cas: str = field(default="")
+    cas: str = field(default=0)
     izbrano: bool = field(default=False)
 
 @dataclass_json
 @dataclass
 class Razdalja:
-    razdalja: int = field(default="")
+    razdalja: int = field(default=0)
     izbrano: bool = field(default=False)
 
 @dataclass_json
 @dataclass
 class VrstaTeka:
-    id: int = field(default="")
+    id: int = field(default=0)
     vrsta: str = field(default="")
 
 @dataclass_json
 @dataclass
 class Tek:
-    id: int = field(default="")
+    id: int = field(default=0)
     tekac: Uporabnik = field(default="")
     vrsta_teka: VrstaTeka = field(default="")
     razdalja: Razdalja = field(default="")
@@ -47,15 +47,15 @@ class Tek:
 @dataclass_json
 @dataclass
 class Tekmovanje:
-    id: int = field(default="")
+    id: int = field(default=0)
     naziv: str = field(default="")
-    leto: int = field(default="")
+    leto: int = field(default=0)
     razdalja: int = field(default="")
 
 @dataclass_json
 @dataclass
 class Rezultat:
-    id: int = field(default="")
+    id: int = field(default=0)
     tekmovanje: Tekmovanje = field(default="")
     priimek_ime: str = field(default="")
     rezultat: str = field(default="")
